7e0bca5ad90961d0904241d20211bf28c94139f1
Chris Rae <clr@chrisrae.com>
2014-05-09 14:59:03 -0700

    Sample chart with scroll and zoom bars for perf testing
    No gestures.

--- chartview.py
+++ chartview.py
@@ -6,26 +6,36 @@ from kivy.app import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.garden.graph import Graph, MeshLinePlot
 from kivy.properties import NumericProperty
+from random import random
 
 from utils import *
 
 Builder.load_file('chartview.kv')
 
-class TestGraph(Graph):
-    def __init__(self, **kwargs):
-        super(TestGraph, self).__init__(**kwargs)
-
-            
 class ChartView(BoxLayout):
     dataPoints = NumericProperty(0)
-    actualData = MeshLinePlot(color=[1, 0, 0, 1])
+    zoomLevel = NumericProperty(100)
+    scrollLevel = NumericProperty(0)
+    dataPlot = MeshLinePlot(color=[1, 0, 0, 1])
     def __init__(self, **kwargs):
         super(ChartView, self).__init__(**kwargs)
+        myChart = kvFind(self, 'rcid', 'testGraph')
+        self.add_test_data()
+        myChart.add_plot(self.dataPlot)
         
     def add_test_data(self):
         myChart = kvFind(self, 'rcid', 'testGraph')
-        #myChart.remove_plot(self.actualData)
         self.dataPoints += 1000
-        self.actualData.points = [(x, sin(x / 100.)) for x in xrange(0, self.dataPoints)]
-        myChart.add_plot(self.actualData)
+        self.dataPlot.points = [(x, random() - random() + sin(x / 100.)) for x in xrange(0, self.dataPoints)]
         myChart.xmax = self.dataPoints
+
+    def zoom(self, howFar):
+        myChart = kvFind(self, 'rcid', 'testGraph')
+        self.zoomLevel = int(howFar)
+        myChart.xmax = myChart.xmin + self.zoomLevel
+
+    def scroll(self, howFar):
+        myChart = kvFind(self, 'rcid', 'testGraph')
+        self.scrollLevel = int(howFar)
+        myChart.xmin = self.scrollLevel
+        myChart.xmax = self.scrollLevel + self.zoomLevel
