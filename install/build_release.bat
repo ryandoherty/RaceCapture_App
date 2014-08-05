a9692efc60f5a33ce8927bb3dbc759416c471ebc
Brent Picasso <brent@autosportlabs.com>
2014-08-04 19:58:18 -0700

    automating building of windows package

--- install/build_release.bat
+++ install/build_release.bat
@@ -6,23 +6,23 @@ IF DEFINED kivy_paths_initialized (GOTO :runkivy)
 ECHO Setting Environment Variables:
 ECHO #################################
 
-set GST_REGISTRY=%kivy_portable_root%gstreamer\registry.bin
+set GST_REGISTRY=%kivy_portable_root%\gstreamer\registry.bin
 ECHO GST_REGISTRY
 ECHO %GST_REGISTRY%
 ECHO ---------------
 
-set GST_PLUGIN_PATH=%kivy_portable_root%gstreamer\lib\gstreamer-1.0
+set GST_PLUGIN_PATH=%kivy_portable_root%\gstreamer\lib\gstreamer-1.0
 ECHO GST_PLUGIN_PATH:
 ECHO %GST_PLUGIN_PATH%
 ECHO ---------------
 
-set PATH=%kivy_portable_root%;%kivy_portable_root%Python27;%kivy_portable_root%tools;%kivy_portable_root%Python27\Scripts;%kivy_portable_root%gstreamer\bin;%kivy_portable_root%MinGW\bin;%PATH%
+set PATH=%kivy_portable_root%;%kivy_portable_root%\Python27;%kivy_portable_root%\tools;%kivy_portable_root%\Python27\Scripts;%kivy_portable_root%\gstreamer\bin;%kivy_portable_root%\MinGW\bin;%PATH%
 ECHO PATH:
 ECHO %PATH%
 ECHO ----------------------------------
 
-set PKG_CONFIG_PATH=%kivy_portable_root%gstreamer\lib\pkgconfig;%PKG_CONFIG_PATH%
-set PYTHONPATH=%kivy_portable_root%kivy;%PYTHONPATH%
+set PKG_CONFIG_PATH=%kivy_portable_root%\gstreamer\lib\pkgconfig;%PKG_CONFIG_PATH%
+set PYTHONPATH=%kivy_portable_root%\kivy;%PYTHONPATH%
 ECHO PYTHONPATH:
 ECHO %PYTHONPATH%
 ECHO ----------------------------------
@@ -33,4 +33,4 @@ ECHO ##################################
 
 :runkivy
 
-buildwininstall.bat
+buildwininstall.bat %1
--- install/buildwininstall.bat
+++ install/buildwininstall.bat
@@ -8,7 +8,7 @@ set str=%str:.=,%
 powershell -Command "(gc win_versioninfo.txt) -replace '!REALVER!', '%str%' | Out-File -encoding ASCII temp_win_versioninfo.txt"
 pyinstaller -y racecapture.spec
 "C:\Program Files (x86)\NSIS\makensis.exe" -DVERSION_STRING="%1" raceCaptureApp.nsi
-dist\racecapture\racecapture
+
 goto end
 :nover
 echo Please specify version number as parameter, e.g.:
