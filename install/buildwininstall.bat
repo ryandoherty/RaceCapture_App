5a8b0f1834596a1bec60dc3edd2328e8efc384ec
Brent Picasso <brent@autosportlabs.com>
2014-08-04 23:19:07 -0700

    updated build script

--- install/buildwininstall.bat
+++ install/buildwininstall.bat
@@ -1,11 +1,11 @@
-@echo off
+rem @echo off
 if %1.==. goto nover
 del dist /f/s/q
 del build /f/s/q
 rem Add the version string to the actual binary
 set str=%1
 set str=%str:.=,%
-powershell -Command "(gc win_versioninfo.txt) -replace '!REALVER!', '%str%' | Out-File -encoding ASCII temp_win_versioninfo.txt"
+powershell -inputformat none -Command "(gc win_versioninfo.txt) -replace '!REALVER!', '%str%' | Out-File -encoding ASCII temp_win_versioninfo.txt"
 pyinstaller -y racecapture.spec
 "C:\Program Files (x86)\NSIS\makensis.exe" -DVERSION_STRING="%1" raceCaptureApp.nsi
 
