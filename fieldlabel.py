8e44fa23a710c2a3c035408c056be9bb9e170da2
Brent Picasso <brent@autosportlabs.com>
2014-09-25 23:54:26 -0700

    adjusting fonts to ASL official font

--- fieldlabel.py
+++ fieldlabel.py
@@ -2,6 +2,7 @@ import kivy
 kivy.require('1.8.0')
 
 from kivy.uix.label import Label
+from kivy.metrics import dp
 
 class FieldLabel(Label):
     def __init__(self, **kwargs):
@@ -9,6 +10,7 @@ class FieldLabel(Label):
         self.bind(width=self.width_changed)
         self.spacing = (20,3)
         self.font_name = "resource/fonts/ASL_light.ttf"
+        self.font_size = dp(20)
         
     def width_changed(self, instance, size):
         self.text_size = (size, None)
