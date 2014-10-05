c7ba862fc77980523d0db1f455eee96aafe23868
Brent Picasso <brent@autosportlabs.com>
2014-10-05 09:51:05 -0700

    raw channels view WIP

--- autosportlabs/racecapture/views/dashboard/rawchannelview.py
+++ autosportlabs/racecapture/views/dashboard/rawchannelview.py
@@ -6,14 +6,38 @@ from kivy.app import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.clock import Clock
 from utils import kvFind
+from autosportlabs.racecapture.views.dashboard.widgets.digitalgauge import DigitalGauge
 
 Builder.load_file('autosportlabs/racecapture/views/dashboard/rawchannelview.kv')
 
+class RawGauge(DigitalGauge):
+    channel = None
+    
+    def __init__(self, **kwargs):
+        super(RawGauge  , self).__init__(**kwargs)
+        self.channel = kwargs.get('channel', self.channel)
+        self.label = self.channel
+            
 class RawChannelView(Screen):
-
+    _gauges = []
+    _grid = None
+    
     def __init__(self, **kwargs):
         super(RawChannelView, self).__init__(**kwargs)
-        self.initView()
-
+       #Clock.schedule_once(lambda dt: self.initView())
+    
+    @property
+    def _gridView(self):
+        if self._grid == None:
+            self._grid = kvFind(self, 'rcid', 'grid')
+        return self._grid
+        
     def initView(self):
-        pass
+        for i in range (1, 30):
+            self._addGauge('channel {}'.format(i))
+
+    def _addGauge(self, channel):
+        gauge = RawGauge(channel=channel)
+        self._gauges.append(gauge)
+        self._gridView.add_widget(gauge)
+        
--- autosportlabs/racecapture/views/dashboard/widgets/digitalgauge.py
+++ autosportlabs/racecapture/views/dashboard/widgets/digitalgauge.py
@@ -1,37 +1,24 @@
 import kivy
 kivy.require('1.8.0')
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
 from kivy.app import Builder
-from collections import OrderedDict  
 from kivy.metrics import dp
-from kivy.graphics import Color
 from utils import kvFind
 from kivy.properties import NumericProperty
+from autosportlabs.racecapture.views.dashboard.widgets.gauge import Gauge
 
 Builder.load_file('autosportlabs/racecapture/views/dashboard/widgets/digitalgauge.kv')
 
-DEFAULT_NORMAL_COLOR = [1, 1, 1, 1]
-DEFAULT_WARNING_COLOR = [1, 0.79, 0 ,1]
-DEFAULT_ALERT_COLOR = [1, 0, 0, 1]
-
-class DigitalGauge(BoxLayout):
+class DigitalGauge(BoxLayout, Gauge):
     value_size = NumericProperty(0)
-    label_size = NumericProperty(0)
+    title_size = NumericProperty(0)
     
     def __init__(self, **kwargs):
         super(DigitalGauge, self).__init__(**kwargs)
         self.value_size = dp(50)
-        self.label_size = dp(25)
+        self.title_size = dp(25)
         self._valueView     = None
         self._labelView     = None
-        self._warning       = 0
-        self._alert         = 0
-        self._value         = 0
-        self._label         = ''
-        self._alertColor    = DEFAULT_ALERT_COLOR
-        self._warningColor  = DEFAULT_WARNING_COLOR
-        self._normalColor   = DEFAULT_NORMAL_COLOR
         self.initWidgets()
             
     def initWidgets(self):
@@ -39,36 +26,8 @@ class DigitalGauge(BoxLayout):
         self.warning = 0
         self.max = 0
                     
-    @property
-    def warning(self):
-        return self._warning
-    
-    @warning.setter
-    def warning(self, value):
-        self._warning = value
-        
-    @property
-    def alert(self):
-        return self._alert
-    
-    @alert.setter
-    def alert(self, value):
-        self._alert = value
-
-    @property
-    def max(self):
-        return self._max
-    
-    @max.setter
-    def max(self, value):
-        self._max = value
-
-    @property
-    def label(self):
-        return self._label
-    
-    @label.setter
-    def label(self, value):
+    def on_title(self, instance, value):
+        x = 1/0
         view = self._labelView
         if not view:
             view = kvFind(self, 'rcid', 'label')
@@ -76,13 +35,7 @@ class DigitalGauge(BoxLayout):
         view.text = str(value)
         self._label = value
         
-        
-    @property
-    def value(self):
-        return self._value
-
-    @value.setter
-    def value(self, value):
+    def on_value(self, instance, value):
         view = self._valueView
         if not view:
             view = kvFind(self, 'rcid', 'value')
@@ -90,12 +43,12 @@ class DigitalGauge(BoxLayout):
 
         self._value = value
         view.text = str(value)
-        if value < self._warning:
-            view.color = self._normalColor
-        elif value < self._alert:
-            view.color = self._warningColor
+        if value < self.warning:
+            view.color = self.normal_color
+        elif value < self.alert:
+            view.color = self.warning_color
         else:
-            view.color = self._alertColor        
+            view.color = self.alert_color        
 
     @property
     def gaugeSize(self):
