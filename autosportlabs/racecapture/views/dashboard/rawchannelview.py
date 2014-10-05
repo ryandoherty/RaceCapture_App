00d3d50ecfde1fd70d31e8a577c8a865152ca50d
Brent Picasso <brent@autosportlabs.com>
2014-10-05 12:24:55 -0700

    finished layout for raw channels view

--- autosportlabs/racecapture/views/dashboard/rawchannelview.py
+++ autosportlabs/racecapture/views/dashboard/rawchannelview.py
@@ -6,16 +6,22 @@ from kivy.app import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
 from kivy.clock import Clock
 from utils import kvFind
+from kivy.properties import ObjectProperty
 from autosportlabs.racecapture.views.dashboard.widgets.digitalgauge import DigitalGauge
 from kivy.properties import StringProperty
+from kivy.graphics import Color, Rectangle
 
 Builder.load_file('autosportlabs/racecapture/views/dashboard/rawchannelview.kv')
 
 NO_DATA_MSG = 'No Data'
 DATA_MSG    = ''
 
+RAW_GRID_BGCOLOR_1 = [0  , 0  , 0  , 1.0]
+RAW_GRID_BGCOLOR_2 = [0.1, 0.1, 0.1, 1.0]
+
 class RawGauge(DigitalGauge):
     
+    backgroundColor = ObjectProperty([0, 0, 0, 0])
     channel = StringProperty(None)
     
     def __init__(self, **kwargs):
@@ -23,10 +29,12 @@ class RawGauge(DigitalGauge):
         
     def on_channel(self, instance, value):
         self.title = str(value)
-            
+        
+        
 class RawChannelView(Screen):
     _gauges = []
     _grid = None
+    _bgCurrent = RAW_GRID_BGCOLOR_1
     
     def __init__(self, **kwargs):
         super(RawChannelView, self).__init__(**kwargs)
@@ -59,10 +67,25 @@ class RawChannelView(Screen):
         statusView = kvFind(self, 'rcid', 'statusMsg')
         statusView.text = NO_DATA_MSG if enabled else DATA_MSG
         
+    def _clearGauges(self):
+        gridView = self._gridView
+        gridView.clear_widgets()
+        self._enableNoDataStatus(True)
+        
     def _addGauge(self, channel, value):
         gauge = RawGauge()
-        self._gauges.append(gauge)
-        self._gridView.add_widget(gauge)
+        gridView = self._gridView
+        
         gauge.value = value
         gauge.channel = channel
+        
+        if len(gridView.children) % 3 == 0:
+            self._bgCurrent = RAW_GRID_BGCOLOR_2 if self._bgCurrent == RAW_GRID_BGCOLOR_1 else RAW_GRID_BGCOLOR_1
+            
+        gauge.backgroundColor = self._bgCurrent
+        gridView.add_widget(gauge)
+        self._gauges.append(gauge)
         self._enableNoDataStatus(False)
+        
+        
+        
