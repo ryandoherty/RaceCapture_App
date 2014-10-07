bd6f530695c215c6a4055c88870b6217f0efce52
Brent Picasso <brent@autosportlabs.com>
2014-10-06 23:18:28 -0700

    initial commit of databus and tests

--- autosportlabs/racecapture/databus/databus.py
+++ autosportlabs/racecapture/databus/databus.py
@@ -1,4 +1,28 @@
 class DataBus(object):
+	channelData = {}
+	channelListeners = {}
+	
 	def __init__(self, **kwargs):
 		super(DataBus, self).__init__(**kwargs)
 	
+	def update(self, channel, value):
+		self.channelData[channel] = value
+		self.notifyListeners(channel, value)
+	
+	def notifyListeners(self, channel, value):
+		listeners = self.channelListeners.get(channel)
+		if not listeners == None:
+			for listener in listeners:
+				listener(value)
+				
+	def getData(self, channel):
+		return self.channelData[channel]
+	
+	def addListener(self, channel, callback):
+		listeners = self.channelListeners.get(channel)
+		if listeners == None:
+			listeners = [callback]
+			self.channelListeners[channel] = listeners
+		else:
+			listeners.append(callback)
+		
