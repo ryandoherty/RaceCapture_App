e340db1ecdc32b380854a2c84146c2a5b6e76d0e
Brent Picasso <brent@autosportlabs.com>
2014-11-23 23:46:50 -0800

    fixed logging of messages

--- autosportlabs/racecapture/api/rcpapi.py
+++ autosportlabs/racecapture/api/rcpapi.py
@@ -139,7 +139,7 @@ class RcpApi:
                 msg = comms.read_message()
                 if msg:
                     
-                    print('msg_rx_worker Rx: ' + str(msg))
+                    #print('msg_rx_worker Rx: ' + str(msg))
                     msgJson = json.loads(msg, strict = False)
                     self.on_rx(True)
                     error_count = 0
