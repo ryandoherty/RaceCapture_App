234b77b1cebc760e204786e8fce2078446e3e6ce
Brent Picasso <brent@autosportlabs.com>
2014-12-20 16:03:51 -0800

    fixed reference to NULL_LAP_TIME

--- autosportlabs/racecapture/views/dashboard/widgets/laptime.py
+++ autosportlabs/racecapture/views/dashboard/widgets/laptime.py
@@ -31,7 +31,7 @@ class Laptime(Gauge):
                 intMinuteValue = int(value)
                 fractionMinuteValue = 60.0 * (value - float(intMinuteValue))
                 if value == MIN_LAP_TIME:
-                    view.text = NULL_LAP_TIME
+                    view.text = self.NULL_LAP_TIME
                 else:
                     view.text = '{}:{}'.format(intMinuteValue,'{0:2.3f}'.format(fractionMinuteValue))
         self.updateColors(view)
