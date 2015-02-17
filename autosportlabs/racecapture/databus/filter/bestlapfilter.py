917e7fd81c7948c25c6b96f5dd4f3889128e14d6
Brent Picasso <brent@autosportlabs.com>
2015-02-17 11:40:45 -0800

    added unit tests for bestlap and lapdelta filters

--- autosportlabs/racecapture/databus/filter/bestlapfilter.py
+++ autosportlabs/racecapture/databus/filter/bestlapfilter.py
@@ -1,11 +1,12 @@
-BEST_LAPTIME_KEY = 'BestLap'
+
 
 class BestLapFilter(object):
     """Update BestLap if laptime is present and is faster than the current best"""
+    BEST_LAPTIME_KEY = 'BestLap'
     best_laptime = 0
     best_laptime_meta = None
-    def __init__(self, channel_metas):
-        self.best_laptime_meta = channel_metas.get_meta(BEST_LAPTIME_KEY)
+    def __init__(self, system_channels):
+        self.best_laptime_meta = system_channels.findChannelMeta(self.BEST_LAPTIME_KEY)
         
     def get_channel_meta(self):
         return self.best_laptime_meta
@@ -19,5 +20,5 @@ class BestLapFilter(object):
             current_best_laptime = self.best_laptime
             if current_best_laptime == 0 or laptime < current_best_laptime: 
                 current_best_laptime = laptime
-                channel_data[BEST_LAPTIME_KEY] = current_best_laptime
+                channel_data[self.BEST_LAPTIME_KEY] = current_best_laptime
                 self.best_laptime = current_best_laptime
---
+++ test/autosportlabs/racecapture/databus/filter/test_bestlapfilter.py
@@ -0,0 +1,48 @@
+import unittest
+from autosportlabs.racecapture.databus.filter.bestlapfilter import BestLapFilter
+from autosportlabs.racecapture.data.sampledata import SystemChannels
+
+class BestLapFilterTest(unittest.TestCase):
+    system_channels = SystemChannels()
+    
+    def test_bestlap_meta(self):
+        sample_filter = BestLapFilter(self.system_channels)
+        meta = sample_filter.get_channel_meta()
+        self.assertIsNotNone(meta)
+        
+    def test_best_lap_default(self):
+        sample_filter = BestLapFilter(self.system_channels)
+        channel_data = {}
+        
+        sample_filter.filter(channel_data)
+        self.assertIsNone(channel_data.get(BestLapFilter.BEST_LAPTIME_KEY))
+
+    def test_first_best_lap(self):
+        sample_filter = BestLapFilter(self.system_channels)
+        channel_data = {'LapTime':111.111}
+        
+        sample_filter.filter(channel_data)
+        self.assertEqual(111.111, channel_data.get(BestLapFilter.BEST_LAPTIME_KEY))
+
+    def test_next_best_lap(self):
+        sample_filter = BestLapFilter(self.system_channels)
+        channel_data = {'LapTime':111.111}
+        
+        sample_filter.filter(channel_data)
+        self.assertEqual(111.111, channel_data.get(BestLapFilter.BEST_LAPTIME_KEY))
+        
+        channel_data['LapTime'] = 110.111
+        sample_filter.filter(channel_data)
+        self.assertEqual(110.111, channel_data.get(BestLapFilter.BEST_LAPTIME_KEY))
+        
+    def test_best_lap_preserved(self):
+        sample_filter = BestLapFilter(self.system_channels)
+        channel_data = {'LapTime':111.111}
+        
+        sample_filter.filter(channel_data)
+        self.assertEqual(111.111, channel_data.get(BestLapFilter.BEST_LAPTIME_KEY))
+        
+        channel_data['LapTime'] = 112.111
+        sample_filter.filter(channel_data)
+        self.assertEqual(111.111, channel_data.get(BestLapFilter.BEST_LAPTIME_KEY))
+        
