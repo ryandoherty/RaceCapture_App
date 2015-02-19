c308eaf7abeda440519969a4a4c282004a4f10e6
Brent Picasso <brent@autosportlabs.com>
2015-02-18 21:15:35 -0800

    find and use whatever zipalign tool is currently present in android sdk

--- build_release_apk.sh
+++ build_release_apk.sh
@@ -5,4 +5,5 @@ buildozer -v android release
 APK=$(ls bin/*.apk)
 RELEASE_APK="${APK/unsigned/signed}"
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore $1 -storepass $2 $APK rcpmobile
-~/.buildozer/android/platform/android-sdk-21/build-tools/19.1.0/zipalign -f 4 $APK $RELEASE_APK
+ZA=`find ~/.buildozer/ -name zipalign -print -quit`
+$ZA -f 4 $APK $RELEASE_APK
