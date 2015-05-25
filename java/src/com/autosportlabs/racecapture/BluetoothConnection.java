d627b5836e193f8626d31f2289695c270d3bf02f
Brent Picasso <brent@autosportlabs.com>
2015-05-25 10:59:10 -0700

    synchronized bluetooth access

--- java/src/com/autosportlabs/racecapture/BluetoothConnection.java
+++ java/src/com/autosportlabs/racecapture/BluetoothConnection.java
@@ -7,7 +7,6 @@ import android.util.Log;
 import java.util.UUID;
 import java.util.Set;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
@@ -17,11 +16,12 @@ import java.lang.reflect.Method;
 public class BluetoothConnection {
 	private static final UUID SerialPortServiceClass_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
 
-	BluetoothSocket socket;
-	InputStream recv_stream;
-	OutputStream send_stream;
-	BufferedReader reader;
+	private volatile BluetoothSocket socket;
+	private volatile InputStream recv_stream;
+	private volatile OutputStream send_stream;
+	private volatile BufferedReader reader;
 	boolean mAllowInsecureConnections = false;
+	Object lock = new Object();
 	
 	static BluetoothConnection g_instance;
 	
@@ -37,7 +37,6 @@ public class BluetoothConnection {
 	
 	private class ConnectThread extends Thread{
 		
-		BluetoothSocket socket = null;
 		BluetoothDevice device = null;
 		String errorMessage = null;
 		
@@ -52,15 +51,15 @@ public class BluetoothConnection {
 	        		Log.i("BluetoothConnection", "create socket insecure mode");
 	        		Method method;
 	        		method = this.device.getClass().getMethod("createRfcommSocket", new Class[] { int.class } );
-	                this.socket = (BluetoothSocket) method.invoke(this.device, 1);  
+	        		BluetoothConnection.this.socket = (BluetoothSocket) method.invoke(this.device, 1);  
 	        	}
 	        	else {
 	        		Log.i("BluetoothConnection", "create socket secure mode");
-	        		this.socket = this.device.createRfcommSocketToServiceRecord( SerialPortServiceClass_UUID );
+	        		BluetoothConnection.this.socket = this.device.createRfcommSocketToServiceRecord( SerialPortServiceClass_UUID );
 	        	}
-	            if (this.socket != null){
+	            if (BluetoothConnection.this.socket != null){
 	            	Log.i("BluetoothConnection", "Got a socket");
-	                this.socket.connect();
+	            	BluetoothConnection.this.socket.connect();
 	            	Log.i("BluetoothConnection", "Socket setup");                
 	            }        	
         	}
@@ -71,61 +70,61 @@ public class BluetoothConnection {
 	}
 	
 	public void open(String port){
-		String error_message = null;
-        try{
-        	BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
-        	adapter.cancelDiscovery();
-        	Set<BluetoothDevice> paired_devices = adapter.getBondedDevices();
-            BluetoothSocket socket = null;
-            for (BluetoothDevice device: paired_devices){
-            	String name = device.getName();
-                if (name.equals(port)){
-                	Log.i("BluetoothConnection", "trying " + name);
-                	
-                    ConnectThread ct = new ConnectThread(device);
-                    ct.start();
-                    ct.join(5000);
-                    socket = ct.socket;
-                    error_message = ct.errorMessage;
-                    break;
-                }
-            }
-            if (socket != null){
-	            this.recv_stream = socket.getInputStream();
-	            this.send_stream = socket.getOutputStream();
-	            this.reader = new BufferedReader(new InputStreamReader(this.recv_stream,"UTF-8"));
-            	this.socket = socket;
-	            Log.i("BluetoothConnection", "Socket Ready");
-            }
-            else{
-                error_message = "Could not detect device " + port + ": " + String.valueOf(error_message);
-            }            
-        }
-        catch(Exception e){
-            error_message = "Error opening Bluetooth socket: " + e.getMessage();
-        }
-            
-        if (this.socket == null){
-            throw new RuntimeException("Error opening Bluetooth port: " + String.valueOf(error_message));
-        }
+		synchronized(this.lock){
+			String error_message = null;
+	        try{
+	        	BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+	        	adapter.cancelDiscovery();
+	        	Set<BluetoothDevice> paired_devices = adapter.getBondedDevices();
+	            for (BluetoothDevice device: paired_devices){
+	            	String name = device.getName();
+	                if (name.equals(port)){
+	                	Log.i("BluetoothConnection", "trying " + name);
+	                	
+	                    ConnectThread ct = new ConnectThread(device);
+	                    ct.start();
+	                    ct.join(5000);
+	                    error_message = ct.errorMessage;
+	                    break;
+	                }
+	            }
+	            if (this.socket != null){
+		            this.recv_stream = this.socket.getInputStream();
+		            this.send_stream = this.socket.getOutputStream();
+		            this.reader = new BufferedReader(new InputStreamReader(this.recv_stream,"UTF-8"));
+		            Log.i("BluetoothConnection", "Socket Ready");
+	            }
+	            else{
+	                error_message = "Could not detect device " + port + ": " + String.valueOf(error_message);
+	            }            
+	        }
+	        catch(Exception e){
+	            error_message = "Error opening Bluetooth socket: " + e.getMessage();
+	        }
+	            
+	        if (this.socket == null){
+	            throw new RuntimeException("Error opening Bluetooth port: " + String.valueOf(error_message));
+	        }
+		}
 	}
 	
 	public void close(){
-        try{
-        	Log.i("BluetoothConnection", "Closing Socket");
-        	if (this.socket != null){
-                this.socket.close();        		
-        	}
-        }
-        catch (Throwable e){
-            //throw new RuntimeException("Error closing Bluetooth socket: " + e.getMessage());        	
-        }
-        finally{
-            this.socket = null;
-            this.recv_stream = null;
-            this.send_stream = null;
-            this.reader = null;
-        }
+		synchronized(this.lock){		
+	        try{
+	        	Log.i("BluetoothConnection", "Closing Socket");
+	        	if (this.socket != null){
+	                this.socket.close();        		
+	        	}
+	        }
+	        catch (Throwable e){
+	        }
+	        finally{
+	            this.socket = null;
+	            this.recv_stream = null;
+	            this.send_stream = null;
+	            this.reader = null;
+	        }
+		}
 	}
 	
 	public boolean isOpen(){
@@ -133,33 +132,36 @@ public class BluetoothConnection {
 	}
 	
 	public String readLine(){
-		try{
-			String line = null;
-			if (this.reader != null){
-				line =  this.reader.readLine();
+		synchronized(this.lock){		
+			try{
+				String line = null;
+				if (this.reader != null){
+					line =  this.reader.readLine();
+				}
+				return line;
 			}
-			return line;
-		}
-		catch(Throwable e){
-			return null;
+			catch(Throwable e){
+				return null;
+			}
 		}
 	}
 	
 	public boolean write(String data){
-		try{
-			if (this.send_stream != null){
-				this.send_stream.write(data.getBytes());
-				return true;
+		synchronized(this.lock){
+			try{
+				if (this.send_stream != null){
+					this.send_stream.write(data.getBytes());
+					return true;
+				}
+				else{
+					return false;
+				}
 			}
-			else{
+			catch(Throwable e){
+				Log.i("BluetoothConnection", "Error writing data " + data + ": " + e.getMessage());
+				this.close();
 				return false;
 			}
-		}
-		catch(Throwable e){
-			Log.i("BluetoothConnection", "Error writing data " + data);
-			this.close();
-			return false;
-			//throw new RuntimeException("Error writing data " + data);
 		}
 	}
 }
