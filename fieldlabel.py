8104a29a377aba5ac46ef792cc90e1898cc890c1
Brent Picasso <brent@autosportlabs.com>
2015-06-04 23:48:34 -0700

    switched to sp()

--- fieldlabel.py
+++ fieldlabel.py
@@ -2,7 +2,7 @@ import kivy
 kivy.require('1.8.0')
 
 from kivy.uix.label import Label
-from kivy.metrics import dp
+from kivy.metrics import sp
 
 class FieldLabel(Label):
     def __init__(self, **kwargs):
@@ -10,7 +10,7 @@ class FieldLabel(Label):
         self.bind(width=self.width_changed)
         self.spacing = (20,3)
         self.font_name = "resource/fonts/ASL_light.ttf"
-        self.font_size = dp(20)
+        self.font_size = sp(20)
         
     def width_changed(self, instance, size):
         self.text_size = (size, None)
