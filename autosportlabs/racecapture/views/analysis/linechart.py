a66396c6aa86544e09bffe66e6787637177b688a
Brent Picasso <brent@autosportlabs.com>
2015-06-24 22:44:46 -0700

    allow mobile touch events to move line chart

--- autosportlabs/racecapture/views/analysis/linechart.py
+++ autosportlabs/racecapture/views/analysis/linechart.py
@@ -50,7 +50,7 @@ class LineChart(ChannelAnalysisWidget):
     
     def add_channel(self, channel_data):
         chart = self.ids.chart
-        plot = LinePlot(color=self._color_sequence.get_next_color(), line_width=1)
+        plot = LinePlot(color=self._color_sequence.get_next_color(), line_width=1.25)
         channel_plot = ChannelPlot(plot, 
                                    channel_data.channel, 
                                    channel_data.min, 
@@ -80,27 +80,20 @@ class LineChart(ChannelAnalysisWidget):
     
     def on_touch_down(self, touch):
         if self.collide_point(touch.x, touch.y):
-            button = touch.button
-            scroll_dir = 0
-            if touch.is_mouse_scrolling:
-                if 'down' in button or 'left' in button:
-                    scroll_dir = 1
-                if 'up' in button or 'right' in button:
-                    scroll_dir = -1
-                print(str(touch) + ' ' + str(scroll_dir))
+            if hasattr(touch, 'button'):
+                button = touch.button
+                scroll_dir = 0
+                if touch.is_mouse_scrolling:
+                    if 'down' in button or 'left' in button:
+                        scroll_dir = 1
+                    if 'up' in button or 'right' in button:
+                        scroll_dir = -1
         super(LineChart, self).on_touch_down(touch)
         return False
         
-    def on_touch_move(self, touch):
-        if not self.collide_point(touch.x, touch.y):
-            return False
-        print(str(touch))
         
-    def on_mouse_pos(self, x, pos):
-        if not self.collide_point(pos[0], pos[1]):
-            return False
-
-        mouse_x = pos[0] - self.pos[0]
+    def dispatch_marker(self, x, y):
+        mouse_x = x - self.pos[0]
         width = self.size[0]
         pct = mouse_x / width
         
@@ -108,3 +101,14 @@ class LineChart(ChannelAnalysisWidget):
             data_index = int(channel_plot.samples * pct)
             marker = MarkerEvent(data_index, channel_plot.sourceref)
             self.dispatch('on_marker', marker)
+        
+    def on_touch_move(self, touch):
+        if not self.collide_point(touch.x, touch.y):
+            return False
+        self.dispatch_marker(touch.x, touch.y)
+        
+    def on_mouse_pos(self, x, pos):
+        if not self.collide_point(pos[0], pos[1]):
+            return False
+        
+        self.dispatch_marker(pos[0], pos[1])
