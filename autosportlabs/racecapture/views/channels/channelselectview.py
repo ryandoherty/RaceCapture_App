7f9f431c993d5690f46710e7096e86dce84431e8
Brent Picasso <brent@autosportlabs.com>
2015-07-08 22:27:50 -0700

    prevent crashing if channelselectview init fails

--- autosportlabs/racecapture/views/channels/channelselectview.py
+++ autosportlabs/racecapture/views/channels/channelselectview.py
@@ -7,6 +7,7 @@ from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.listview import ListView, ListItemButton
 from utils import kvFind
 from kivy.adapters.listadapter import ListAdapter
+from kivy.logger import Logger
 
 Builder.load_file('autosportlabs/racecapture/views/channels/channelselectview.kv')
 
@@ -27,27 +28,30 @@ class ChannelSelectView(FloatLayout):
         
         data = []
         channel_list = self.ids.channelList
-        for available_channel,channelMeta in settings.runtimeChannels.channels.iteritems():
-            channel_type = channelMeta.type
-            data.append({'text': available_channel, 'is_selected': False})
-            
-        args_converter = lambda row_index, rec: {'text': rec['text'], 'size_hint_y': None, 'height': dp(50)}
-
-        list_adapter = ListAdapter(data=data,
-                           args_converter=args_converter,
-                           cls=ChannelItemButton,
-                           selection_mode='single',
-                           allow_empty_selection=True)
-
-        channel_list.adapter=list_adapter
-        list_adapter.bind(on_selection_change=self.on_select)
-        self.channel = channel
-
+        try:
+            for available_channel,channelMeta in settings.runtimeChannels.channels.iteritems():
+                channel_type = channelMeta.type
+                data.append({'text': available_channel, 'is_selected': False})
+                
+            args_converter = lambda row_index, rec: {'text': rec['text'], 'size_hint_y': None, 'height': dp(50)}
+    
+            list_adapter = ListAdapter(data=data,
+                               args_converter=args_converter,
+                               cls=ChannelItemButton,
+                               selection_mode='single',
+                               allow_empty_selection=True)
+    
+            channel_list.adapter=list_adapter
+            list_adapter.bind(on_selection_change=self.on_select)
+            self.channel = channel
+        except Exception as e:
+            Logger.error("ChannelSelectView: Error initializing: " + str(e))
+    
     def on_select(self, value):
         try:
             self.channel = value.selection[0].text
         except Exception as e:
-            print('Error Selecting channel: ' + str(e))
+            Logger.error('ChannelSelectView: Error Selecting channel: ' + str(e))
     
     def on_close(self):
         self.dispatch('on_channel_selected', self.channel)
