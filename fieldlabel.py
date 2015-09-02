4867e3065c20944bd0dd938358fed3dacb2854f6
Brent Picasso <brent@autosportlabs.com>
2015-09-02 10:10:00 -0700

    prevent labels from wrapping

--- fieldlabel.py
+++ fieldlabel.py
@@ -11,6 +11,8 @@ class FieldLabel(Label):
         self.spacing = (20,3)
         self.font_name = "resource/fonts/ASL_light.ttf"
         self.font_size = sp(20)
+        self.shorten = True
+        self.shorten_from = 'right'
         
     def width_changed(self, instance, size):
         self.text_size = (size, None)
