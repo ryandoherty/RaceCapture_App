c6ee798bd3bcba6ef9bbb0e026e118f6c77652f0
Brent Picasso <brent@autosportlabs.com>
2015-12-31 19:41:30 -0800

    settled on one color sequence

--- autosportlabs/uix/color/colorsequence.py
+++ autosportlabs/uix/color/colorsequence.py
@@ -1,9 +1,6 @@
 from kivy.utils import get_color_from_hex as rgb
 
-#DEFAULT_COLOR_SEQUENCE = ['FFFFFF', '003AC1','FF5D00','FFC700','607DC1','FFAE7F','FFE37F','919FC1','FFD6BF','FFF1BF']
-DEFAULT_COLOR_SEQUENCE = ['FFFFFF', '8A00B8', '3366FF', 'F5B800', '8AB800']
-#DEFAULT_COLOR_SEQUENCE = ['FFFFFF', '5DA5DA', 'FAA43A','60BD68', 'F17CB0', 'B2912F', 'B276B2','DECF3F', 'F15854', 'FFFFFF']
-#DEFAULT_COLOR_SEQUENCE =  ['2b908f', '90ee7e', 'f45b5b', '7798BF', 'aaeeee', 'ff0066', 'eeaaee', '55BF3B', 'DF5353', '7798BF', 'aaeeee']
+DEFAULT_COLOR_SEQUENCE = ['FFFFFF', '8A00B8', '3366FF', 'F5B800', '8AB800', 'f45b5b', 'ff0066']
 
 class ColorSequence(object):
     color_index = 0
