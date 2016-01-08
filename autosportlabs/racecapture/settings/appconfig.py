ac1a05e62db4ec6a5cc5564be503e8b95eeedd2f
Brent Picasso <brent@autosportlabs.com>
2016-01-08 12:57:37 -0800

    replaced print with logging

--- autosportlabs/racecapture/settings/appconfig.py
+++ autosportlabs/racecapture/settings/appconfig.py
@@ -1,7 +1,8 @@
+from kivy.logger import Logger
 class AppConfig(object):
     userDir = "."
     
     def setUserDir(self, userDir):
-        print('using user storage directory: ' + userDir)
+        Logger.info('AppConfig: using user storage directory: {}'.format(userDir))
         self.userDir = userDir
         pass
