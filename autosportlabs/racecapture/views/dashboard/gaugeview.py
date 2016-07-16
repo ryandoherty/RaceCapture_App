ab99fd392bf3701b1628d70c81b45f6db40fc024
Brent Picasso <brent@autosportlabs.com>
2016-07-16 11:39:31 -0700

    moved back to .kv layout for gauges

--- autosportlabs/racecapture/views/dashboard/gaugeview.py
+++ autosportlabs/racecapture/views/dashboard/gaugeview.py
@@ -11,7 +11,51 @@ from autosportlabs.racecapture.views.dashboard.widgets.gauge import Gauge
 from autosportlabs.racecapture.views.dashboard.widgets.digitalgauge import DigitalGauge
 from autosportlabs.racecapture.views.dashboard.dashboardscreen import DashboardScreen
 
+GAUGE_VIEW_KV = """
+<GaugeView>:
+    AnchorLayout:
+
+        AnchorLayout:
+            anchor_x:'left'
+            anchor_y:'top'
+            RoundGauge:
+                size_hint_x: 0.3
+                size_hint_y: 0.5
+                id: tl
+
+        AnchorLayout:
+            anchor_x:'left'
+            anchor_y:'bottom'
+            RoundGauge:
+                size_hint_x: 0.3
+                size_hint_y: 0.5
+                id: bl
+        
+        AnchorLayout:
+            anchor_x:'right'
+            anchor_y:'center'
+            BoxLayout:
+                size_hint_x: 0.3
+                orientation: 'vertical'
+                RoundGauge:
+                    id: tr
+                RoundGauge:
+                    id: br
+                    
+                    
+        AnchorLayout:
+            anchor_x:'center'
+            anchor_y:'center'
+            BoxLayout:
+                size_hint_x: 0.47
+                orientation: 'vertical'
+                RoundGauge:
+                    id: center
+"""
+
 class GaugeView(DashboardScreen):
+
+    Builder.load_string(GAUGE_VIEW_KV)
 
     def __init__(self, databus, settings, **kwargs):
         super(GaugeView, self).__init__(**kwargs)
@@ -19,11 +63,8 @@ class GaugeView(DashboardScreen):
         self._settings = settings
         self._initialized = False
 
-    def pct(self, p):
-        return pct_h(p)
-
     def on_meta(self, channelMetas):
-        gauges = self.findActiveGauges()
+        gauges = self._find_active_gauges()
 
         for gauge in gauges:
             channel = gauge.channel
@@ -34,48 +75,22 @@ class GaugeView(DashboardScreen):
                     gauge.min = channelMeta.min
                     gauge.max = channelMeta.max
 
-    def findActiveGauges(self):
+    def _find_active_gauges(self):
         return list(kvFindClass(self, Gauge))
 
     def on_enter(self):
         if self._initialized == False:
             self._init_view()
 
-    def _load_gauges(self):
-        base = AnchorLayout()
-        self.add_widget(base)
-
-        top_left = AnchorLayout(anchor_x='left', anchor_y='top')
-        top_left.add_widget(RoundGauge(size_hint_x=0.3, size_hint_y=0.5, rcid='tl'))
-        base.add_widget(top_left)
-
-        bottom_left = AnchorLayout(anchor_x='left', anchor_y='bottom')
-        bottom_left.add_widget(RoundGauge(size_hint_x=0.3, size_hint_y=0.5, rcid='bl'))
-        base.add_widget(bottom_left)
-
-        top_right = AnchorLayout(anchor_x='right', anchor_y='top')
-        top_right.add_widget(RoundGauge(size_hint_x=0.3, size_hint_y=0.5, rcid='tr'))
-        base.add_widget(top_right)
-
-        bottom_right = AnchorLayout(anchor_x='right', anchor_y='bottom')
-        bottom_right.add_widget(RoundGauge(size_hint_x=0.3, size_hint_y=0.5, rcid='br'))
-        base.add_widget(bottom_right)
-
-        center = AnchorLayout(anchor_x='center', anchor_y='center')
-        center.add_widget(RoundGauge(size_hint_x=0.47, rcid='center'))
-        base.add_widget(center)
-
-
     def _init_view(self):
-        self._load_gauges()
         dataBus = self._databus
         dataBus.addMetaListener(self.on_meta)
 
-        gauges = self.findActiveGauges()
+        gauges = self._find_active_gauges()
         for gauge in gauges:
             gauge.settings = self._settings
             gauge.data_bus = dataBus
-            channel = self._settings.userPrefs.get_gauge_config(gauge.rcid)
+            channel = self._settings.userPrefs.get_gauge_config(gauge.id)
             if channel:
                 gauge.channel = channel
 
